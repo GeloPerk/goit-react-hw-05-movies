538e79bf4fd69c5abb806508139ad314c3395e18
GeloPerk <114014987+GeloPerk@users.noreply.github.com>
2023-04-11 11:41:23 +0300

    lets get started

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { lazy } from 'react';
+import Layout from './Layout/Layout';
+import Home from '../pages/Home';
+
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetail = lazy(() => import('../pages/MovieDetail'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
+export const App = () => {
+  return (
+    <div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MovieDetail />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
